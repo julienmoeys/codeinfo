ed3045024c5d07e4fb8a847244751b2ee5e38719
Julien Moeys <julienmoeys@yahoo.fr>
2019-01-10 15:11:36 +0100

    Added .onAttach() to the package

--- R/codeinfo.r
+++ R/codeinfo.r
@@ -10,6 +10,44 @@
 
 
 
+# .onAttach ================================================
+
+#'@importFrom utils packageVersion
+NULL
+
+.onAttach <- function(# Internal. Message displayed when loading the package.
+ libname, 
+ pkgname  
+){  
+    # muPar( "timeSeriesValid" = isValidTimeSeries ) 
+    
+    # Welcome message
+    if( interactive() ){ 
+        gitVersion <- system.file( "GIT_REVISION", package = pkgname ) 
+        
+        if( gitVersion != "" ){ 
+            gitVersion <- readLines( con = gitVersion )[ 1L ] 
+            gitVersion <- strsplit( x = gitVersion, split = " ", 
+                fixed = TRUE )[[ 1L ]][ 1L ]
+            
+            gitVersion <- sprintf( "(git revision: %s)", gitVersion ) 
+        }else{ 
+            gitVersion <- "(git revision: ?)" 
+        }   
+        
+        msg <- sprintf( 
+            "%s %s %s. For help type: help(pack='%s')", 
+            pkgname, 
+            as.character( utils::packageVersion( pkgname ) ), 
+            gitVersion, # svnVersion
+            pkgname ) 
+        
+        packageStartupMessage( msg ) 
+    }   
+}   
+
+
+
 #' Compute the MD5 checksums of each files in an installed package and the checksum of the checksums
 #'
 #' Compute the \code{\link[tools:md5sum]{MD5 checksums}} of 
